ca2bd847222ff0da851393cfe581cc09ecf2d106
Manish Chauhan <manish.chauhan@ushatech.in>
2024-10-09 16:58:55 +0530

    Issues with sharing variables without synchronization

--- src/main/java/in/ushatech/NoVisibility.java
+++ src/main/java/in/ushatech/NoVisibility.java
@@ -1,15 +1,16 @@
 package in.ushatech;
 
+// Weird things can happen in multithreaded environment if
 public class NoVisibility
 {
-    private static boolean ready;
-    private static int number;
+    private static boolean ready; // stale data
+    private static int number;// stale data
 
     private static class ReaderThread extends Thread
     {
         public void run()
         {
-            while(!ready)
+            while(!ready) // potential for infinite loops
             {
                 Thread.yield();
             }
@@ -21,7 +22,8 @@ public class NoVisibility
     {
         new ReaderThread().start();
         number = 42 ;// line 1
-        ready = true;// line 2
+        ready = true;// line 2 ready might never be visible to the thread if the cache saves the false
+        // value for ready
         // order of line 1 and line 2 is not guaranteed
         // cache , reordering can occur
     }
